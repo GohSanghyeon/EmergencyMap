c7a1f776f05a390969499080258a1376c9a21af7
Jyeontae <jyt5768@hanmail.net>
2019-11-26 01:08:49 +0900

    json 통신

--- app/src/main/java/com/example/emergencymap/MainActivity.kt
+++ app/src/main/java/com/example/emergencymap/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.emergencymap
 
 import android.Manifest
+import android.content.ContentValues
 import android.os.Bundle
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -10,6 +11,8 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.content.Intent
+import android.os.AsyncTask
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
@@ -18,14 +21,30 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.isVisible
 import androidx.core.view.iterator
 import com.example.emergencymap.notshowing.LocationProvider
+import com.google.android.youtube.player.internal.s
+import com.google.android.youtube.player.internal.u
+import com.google.android.youtube.player.internal.x
+import com.google.android.youtube.player.internal.y
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
+import kotlinx.android.synthetic.main.activity_location_list.*
 import kotlinx.android.synthetic.main.activity_main.*
 //import kotlinx.android.synthetic.main.app_bar_main.*
 //import kotlinx.android.synthetic.main.fragment_home.*
 import org.jetbrains.anko.toast
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.io.Reader
+import java.lang.Exception
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLConnection
 
 val permissionUsing: Array<out String> = arrayOf(
     Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -36,11 +55,11 @@ val permissionUsing: Array<out String> = arrayOf(
     get() = field.clone()
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var locationSource: LocationProvider
     private var map: NaverMap? = null
-
+    lateinit var url : URL
+    var data : String? = null
     companion object{
         //for permission check
         private const val STARTING = 10000
@@ -174,10 +193,38 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 textTestWest.text = "맵 서단 : ${coordinationBoundary.westLongitude}"
                 textTestSouth.text = "맵 남단 : ${coordinationBoundary.southLatitude}"
                 textTestNorth.text = "맵 북단 : ${coordinationBoundary.northLatitude}"
+                 var surl : String= "http://15.164.116.17/xy.php?east=${coordinationBoundary.eastLongitude}" +
+                            "&west=${coordinationBoundary.westLongitude}&south=${coordinationBoundary.southLatitude}&" +
+                            "north=${coordinationBoundary.northLatitude}"
+                url = URL(surl)
+
+                Log.d("line", url.toString())
+                val networkTask = NetworkTask(surl, null)
+                networkTask.execute()
+                try {
+                    if (data != null) {
+                        val jObject = JSONObject(data)
+                        val jArray = jObject.getJSONArray("123")
+                        for (i in 0 until jArray.length()) {
+                            val obj = jArray.getJSONObject(i)
+                            val x = obj.getDouble("x")
+                            //val y = obj.getDouble("y")
+                            Log.d("line x=", "$x\n")
+                            //Log.d("line y=", "$y\n")
+                        }
+                    }
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
             }
         }
-
-
+    }
+    fun readStream(inputStream: InputStreamReader): String {
+        val bufferedReader = BufferedReader(inputStream)
+        val stringBuffer = StringBuffer("")
+        bufferedReader.forEachLine { stringBuffer.append(it) }
+        Log.d("dusxo", stringBuffer.toString())
+        return stringBuffer.toString().replaceFirst("\uFEFF\uFEFF", "")
     }
 
     private fun setMapToNowLocation(){
@@ -186,6 +233,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 map?.moveCamera(CameraUpdate.scrollTo(LatLng(it)))
                 toast("조회 완료")
             }
+        }
+    }
+    inner class NetworkTask(private val url: String, private val values: ContentValues?) :
+        AsyncTask<Void, Void, String>() {
+
+        override fun doInBackground(vararg params: Void): String? {
+
+            val result: String? // 요청 결과를 저장할 변수.
+            val requestHttpURLConnection = RequestHttpURLConnection()
+            result = requestHttpURLConnection.request(url, values) // 해당 URL로 부터 결과물을 얻어온다.
+
+            return result
+        }
+
+        override fun onPostExecute(s: String){
+            super.onPostExecute(s)
+            //doInBackground()로 부터 리턴된 값이 onPostExecute()의 매개변수로 넘어오므로 s를 출력한다.
+            Log.d("line", "$s\n")
+            data = s
         }
     }
 }
