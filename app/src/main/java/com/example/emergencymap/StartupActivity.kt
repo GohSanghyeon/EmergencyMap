8396f8b3c9926ef82d9529914ee8932e9a9167e0
paad112233@naver.com <paad112233@naver.com>
2019-12-07 09:21:09 +0900

    StartupActivity코드 인터넷 연결 안되었을때 작동하도록 수정 누락된 RegionInfoDownloader파일 추가

--- app/src/main/java/com/example/emergencymap/StartupActivity.kt
+++ app/src/main/java/com/example/emergencymap/StartupActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.ConnectivityManager
 import android.net.Network
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.emergencymap.notshowing.RegionInfoDownloader
 import kotlinx.android.synthetic.main.activity_startup.*
@@ -12,31 +13,89 @@ import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.toast
 
 class StartupActivity : AppCompatActivity(){
+    companion object{
+        const val MILLISEC_CHECKING_DISCONNECTED = 5000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_startup)
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
 
-        textNowWorking.text = "인터넷 연결 상태 확인중..."
         val netMonitor = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        netMonitor.registerDefaultNetworkCallback(
-            object : ConnectivityManager.NetworkCallback(){
-            override fun onAvailable(network: Network) {
-                netMonitor.unregisterNetworkCallback(this)
-                toast("Network : On-line")
 
-                textNowWorking.text = "지역별 정보를 다운로드 중..."
-                RegionInfoDownloader(this@StartupActivity){
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+        textNowWorking.text = "인터넷 연결 상태 확인중..."
+
+        val callbacks = CallbacksForCheckingInternet(
+            netMonitor,
+            doingIsConnected = {
+                runOnUiThread {
+                    toast("Network : On-line")
+                    textNowWorking.text = "지역별 정보를 다운로드 중..."
+                }
+
+                RegionInfoDownloader(this@StartupActivity) {
                     startActivity<MainActivity>()
                 }.execute()
             }
+            , doingIsDisconnected = {
+                //Todo : 인터넷이 Offline일때 할 것
 
-            override fun onUnavailable() {
-                netMonitor.unregisterNetworkCallback(this)
-                toast("Network : Off-line")
-                //TODO : 인터넷 연결이 되지 않았을때 초기화면 액티비티 실행하기
+                runOnUiThread {
+                    toast("Network : Off-line")
+                    textNowWorking.text = "이제 오프라인 때 나오는 액티비티로 전환해야 함"
+                    progressBar.visibility = View.INVISIBLE
+                }
+            })
 
+        netMonitor.registerDefaultNetworkCallback(callbacks)
+        callbacks.timerForCheckStillDisconnected.completedCallbackRegistering()
+        callbacks.timerForCheckStillDisconnected.start()
+    }
+
+    private class CallbacksForCheckingInternet(
+        private val netMonitor: ConnectivityManager
+        , private val doingIsConnected : (() -> Unit)
+        , private val doingIsDisconnected : (() -> Unit)
+    ) : ConnectivityManager.NetworkCallback(){
+        val timerForCheckStillDisconnected = TimerForDisconnection()
+
+        override fun onAvailable(network: Network) {
+            if (timerForCheckStillDisconnected.executedDisconnected)
+                return
+            else
+                timerForCheckStillDisconnected.connectedSoDoNotExecute()
+
+            //if netMonitor is null, blow up this app
+            netMonitor.unregisterNetworkCallback(this)
+            doingIsConnected()
+        }
+
+        inner class TimerForDisconnection: Thread() {
+            private var registeredCallbackConnected = false
+            private var isStillDisconnected = true
+            var executedDisconnected = false
+                private set
+
+            override fun run() {
+                sleep(MILLISEC_CHECKING_DISCONNECTED)
+
+                executedDisconnected = true
+                if (isStillDisconnected) {
+                    if (this != null && registeredCallbackConnected)
+                        netMonitor.unregisterNetworkCallback(this@CallbacksForCheckingInternet)
+
+                    doingIsDisconnected()
+                }
             }
-        })
+
+            fun connectedSoDoNotExecute() {
+                isStillDisconnected = false
+            }
+
+            fun completedCallbackRegistering() {
+                registeredCallbackConnected = true
+            }
+        }
     }
 }
